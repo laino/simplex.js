c5b71a3c2c1d876fbaa4bcb4bf4ba025ab4896ef
Laino <lain@volafile.org>
2021-12-18 01:08:30 +0100

    ...

---
+++ README.md
@@ -0,0 +1,1 @@
+Quick and dirty simplex solver in JS. Don't use.
---
+++ simplex.js
@@ -0,0 +1,114 @@
+class Simplex {
+    constructor() {
+        this.constraints = [];
+    }
+
+    addUpperConstraint(bound, ... x) {
+        this.constraints.push([
+            bound,
+            ... x.map(x => -x)
+        ]);
+    }
+
+    addLowerConstraint(bound, ... x) {
+        this.constraints.push([
+            bound,
+            ... x
+        ]);
+    }
+
+    maximum(... objective) {
+        const constraints = this.constraints;
+        const variables = objective.length + constraints.length;
+
+        const pad = Array(constraints.length).fill(0);
+        const system = [];
+
+        for (let i = 0; i < constraints.length; i++) {
+            const constraint = constraints[i];
+            system.push([i + objective.length + 2, ... constraint, ... pad]);
+        }
+
+        system.push([0, 0, ... objective, ... pad])
+
+        while (true) {
+            const obj = system[system.length - 1];
+
+            let c = 2;
+            for (; c < obj.length; c++) {
+                if (obj[c] > 0) {
+                    break;
+                }
+            }
+
+            if (c === obj.length) {
+                break;
+            }
+
+            let r = -1;
+            let strictest = Number.MAX_VALUE;
+
+            for (let i = 0; i < constraints.length; i++) {
+                const row = system[i];
+
+                if (row[c] >= 0) {
+                    continue;
+                }
+
+                const strictness = -row[1] / row[c];
+
+                if (strictness < strictest) {
+                    strictest = strictness;
+                    r = i;
+                }
+            }
+
+            if (r === -1) {
+                // unbounded
+                return {
+                    max: Infinity,
+                    c: null
+                };
+            }
+
+            const row = system[r];
+
+            for (let i = 2; i < row.length; i++) {
+                row[i] /= row[c];
+            }
+
+            row[0] = c;
+            row[c] = 0;
+            row[1] = strictest;
+
+            for (let i = 0; i < system.length; i++) {
+                const subRow = system[i];
+                const m = subRow[c];
+                for (let k = 1; k < subRow.length; k++) {
+                    subRow[k] += row[k] * m;
+                }
+                subRow[c] = 0;
+            }
+        }
+
+        const co = Array(objective.length).fill(0);
+
+        for (let i = 0; i < system.length - 1; i++) {
+            const row = system[i];
+            if (row[0] - 2 < objective.length) {
+                co[row[0] - 2] = row[1];
+            }
+        }
+
+        return {
+            co,
+            max: system[system.length - 1][1]
+        }
+    }
+}
+
+const s = new Simplex();
+
+s.addUpperConstraint(5, -1, -1);
+
+console.log(s.maximum(1, 1));
